e90a3cab30844e53288b1337d49808fe5addffd5
Your Name <harrishtet.nyanlinhtet@gmail.com>
2026-02-21 01:24:54 -0800

    update

--- convolution.py
+++ convolution.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Convolution():
-    def __init__(self, kernel_shape = (2,2), num_kernels=2):
+    def __init__(self, kernel_shape = (2,2), num_kernels=1):
         self.kernel_shape = kernel_shape
         self.depth = num_kernels
         self.kernels = np.random.randn(num_kernels,*self.kernel_shape)
@@ -11,11 +11,13 @@ class Convolution():
         #input matrix shape (height, width)
         input_height, input_width = input_data.shape
         #output matrix shape (num_kernels, output_height, output_width)
-        output_height = input_height - self.kernel_shape[0] + 1
-        output_width = input_width - self.kernel_shape[1] + 1
+        output_height = input_height
+        output_width = input_width
         #create output matrix
         self.output_shape = (self.depth, output_height, output_width)
         output = np.zeros(self.output_shape)
+        #pad input data with zeroes around the borders
+        np.pad(input_data, 1, mode='constant', constant_values=0)
         #perform convolution
         for k in range(self.output_shape[0]):
             for i in range(output_height):
@@ -23,7 +25,41 @@ class Convolution():
                     region = input_data[i:i+self.kernel_shape[0], j:j+self.kernel_shape[1]]
                     output[k, i, j] = np.sum(region * self.kernels[k]) + self.biases[k, 0, 0]
         return output
+
+    def max_pool(self, input_data, pool_size=(2,2), stride=(2,2)):
+        #input matrix shape (num_kernels, height, width)
+        num_kernels, input_height, input_width = input_data.shape
+        #output matrix shape (num_kernels, output_height, output_width)
+        output_height = input_height // pool_size[0]
+        output_width = input_width // pool_size[1]
+        #create output matrix
+        self.output_shape = (num_kernels, output_height, output_width)
+        output = np.zeros(self.output_shape)
+        #perform max pooling
+        for k in range(num_kernels):
+            for i in range(output_height):
+                for j in range(output_width):
+                    region = input_data[k, i*pool_size[0]:(i+1)*pool_size[0], j*pool_size[1]:(j+1)*pool_size[1]]
+                    output[k, i, j] = np.max(region)
+        return output
+    
+    def flatten(self, input_data):
+        output = input_data.flatten().reshape(-1, 1)
+        return output
     
 Convolution_instance = Convolution()
 convu = Convolution_instance.forward(np.random.randn(10,10))
-print(np.maximum(0, convu))
+relu = np.maximum(0, convu)
+maxpooled = Convolution_instance.max_pool(relu)
+flattened = Convolution_instance.flatten(maxpooled)
+print(flattened.shape)
+
+# for convo in maxpooled:
+#     for row in convo:
+#         for cell in row:
+#             print(f'{cell:.2f}', end=' ')
+#         print()
+
+# for flattened_value in flattened:
+#     print(f'{flattened_value[0]:.2f}', end=' ')
+
